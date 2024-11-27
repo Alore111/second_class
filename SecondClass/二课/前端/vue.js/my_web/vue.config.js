aae8e2a5378483477b652da8b8a4910d5ad12e62
Allllore <2050791391@qq.com>
2024-11-27 20:12:47 +0800

    feat: 配置前端代理，可替代nginx

--- SecondClass/二课/前端/vue.js/my_web/vue.config.js
+++ SecondClass/二课/前端/vue.js/my_web/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 配置前端服务地址和端口
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    // 配置代理
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',//后端服务地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+  }
 })
